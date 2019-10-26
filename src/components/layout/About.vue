2ad5682b4a5a1356e701298e3728b3633973bf88
Hoang Liem Nguyen <daniel.liemng@gmail.com>
2019-10-26 12:28:42 -0400

    Add about

--- src/components/layout/About.vue
+++ src/components/layout/About.vue
@@ -4,8 +4,9 @@
       <h2 class="my-4 display-3 pink--text text-center">About</h2>
       <v-card flat tile class="grey lighten-3 white--text text-center">
         <v-card-text>
-          <p>This is the simple BookShelf app built with Vue, Vuetify and Firestore.</p>
-          <p>Users can do the CRUD functionality</p>
+          <p class="black--text">This is the simple BookShelf app built with Vue, Vuetify and Firebase/Firestore.</p>
+          <p class="black--text">Users need to register or login to access the dashboard.</p>
+          <p class="black--text">Users can create, update, view and delete a book </p>
         </v-card-text>
       </v-card>
     </v-container>
